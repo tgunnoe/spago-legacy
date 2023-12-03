e01dc082b570481d3ece80838d9165964ea94cf4
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2023-12-03 18:13:27 +0200

    Fix npm download URL

--- npm/install.js
+++ npm/install.js
@@ -4,6 +4,6 @@ const fetch = require("make-fetch-happen");
 const tar = require("tar");
 const version = "0.21.0";
 const platform = { win32: "Windows", darwin: "macOS" }[process.platform] || "Linux";
-const url = `https://github.com/purescript/spago/releases/download/${version}/${platform}.tar.gz`
+const url = `https://github.com/purescript/spago-legacy/releases/download/${version}/${platform}.tar.gz`
 
 fetch(url).then(res => res.body.pipe(tar.x({ "C": './' })));
