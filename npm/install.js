bd62a15befb218b2a93fa3b90316f71e12033820
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2023-12-03 18:11:26 +0200

    Prepare npm release as spago-legacy

--- npm/install.js
+++ npm/install.js
@@ -2,8 +2,8 @@
 
 const fetch = require("make-fetch-happen");
 const tar = require("tar");
-const version = "PACKAGE_VERSION";
+const version = "0.21.0";
 const platform = { win32: "Windows", darwin: "macOS" }[process.platform] || "Linux";
 const url = `https://github.com/purescript/spago/releases/download/${version}/${platform}.tar.gz`
 
-fetch(url).then(res => res.body.pipe(tar.x({"C": './'})));
+fetch(url).then(res => res.body.pipe(tar.x({ "C": './' })));
